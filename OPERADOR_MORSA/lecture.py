ea610aa3b1f2d49ceb09a62579b2dc3d21c6262c
bretmer <condorihuamanbrink05@gmail.com>
2024-05-03 15:33:59 -0500

    cree mi archivo lecture.py del operador morsa

---
+++ OPERADOR_MORSA/lecture.py
@@ -0,0 +1,20 @@
+## Ejercicios
+
+# ejemplo 1
+# crear un programa que pida la nota del alumno
+nota_alumno:int=int(input("ingrese su nota: "))
+print("aprobado" if (nota_alumno)>=13 else "desaprobado" )
+
+print("---------------------------")
+
+# ejemplo 2
+# crear un programa que pida su edad a un usuario
+edad_usuario:int=int(input("ingrese su edad: "))
+print("ya eres mayor de edad" if (edad_usuario)>=18 else "eres menor de edad" )
+
+print("---------------------------")
+
+# ejemplo 3
+numero_entero:int=int(input("ingrese un numero: "))
+print("el numero es par"  if (numero_entero)%2 ==0 else "el numero es impar")
+
